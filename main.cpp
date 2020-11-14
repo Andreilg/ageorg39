5e29b87114a981e4c314e31075ec1e4bfbc059ce
Andrei Liviu Georgescu <github@andreigeorgescu.com>
2020-11-14 01:15:02 -0500

    Add usage check

--- main.cpp
+++ main.cpp
@@ -1,1 +1,9 @@
+#include <iostream>
+#include "functions.h"
 
+int main(int argc, char *argv[]) {
+    if (argc != args) {
+        std::cerr << "usage: ./cache-sim input_trace.txt output.txt" << std::endl;
+        return 1;
+    }
+}
